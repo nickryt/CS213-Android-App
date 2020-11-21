f32b4a3433582aa5e21076204d9e491e74759acc
Nick Rytelewski <rytnick@gmail.com>
2020-11-21 16:10:05 -0500

    Update NJStateMuseum.java

--- app/src/main/java/com/example/pa5/NJStateMuseum.java
+++ app/src/main/java/com/example/pa5/NJStateMuseum.java
@@ -3,13 +3,23 @@ package com.example.pa5;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NJStateMuseum extends AppCompatActivity {
+
+
+    private int SENIOR_PRICE = 17;
+    private int STUDENT_PRICE = 12;
+    private int ADULT_PRICE = 25;
+
+    private Button ticketPrices;
+    private TextView ticketCost;
+    private TextView taxCost;
+    private TextView totalCost;
 
     private ImageButton MuseumPicture;
     private TextView name;
@@ -35,6 +45,8 @@ public class NJStateMuseum extends AppCompatActivity {
         spinnerStudent.setAdapter(adapter);
         spinnerAdult.setAdapter(adapter);
 
+        ticketPrices = findViewById(R.id.calculateButton);
+
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,17 +55,26 @@ public class NJStateMuseum extends AppCompatActivity {
             }
         });
 
+        ticketPrices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                int price = (SENIOR_PRICE * Integer.parseInt(spinner.getSelectedItem().toString()));
+                price += (STUDENT_PRICE * Integer.parseInt(spinnerStudent.getSelectedItem().toString()));
+                price += (ADULT_PRICE * Integer.parseInt(spinnerAdult.getSelectedItem().toString()));
 
 
+                ticketCost = findViewById(R.id.price_amount);
+                taxCost = findViewById(R.id.tax_amount);
+                totalCost = findViewById(R.id.total_amount);
 
+                ticketCost.setText("$" + price);
 
+                Double tax = price * 0.06625;
+                //ticketCost.setText("$" + String.format("%.2f", price));
+                taxCost.setText("$" + String.format("%.2f", tax));
+                totalCost.setText("$" + String.format("%.2f", price + tax));
 
-
-
-
+            }
+        });
     }
-
-
-
-
 }
