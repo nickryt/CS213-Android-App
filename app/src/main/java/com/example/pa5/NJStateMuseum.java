cfeb8a48ece7c4c8aba70a8b2e5c8930adc19a03
Nick Rytelewski <rytnick@gmail.com>
2020-11-21 15:15:13 -0500

    dfgfdg

--- app/src/main/java/com/example/pa5/NJStateMuseum.java
+++ app/src/main/java/com/example/pa5/NJStateMuseum.java
@@ -1,7 +1,9 @@
 package com.example.pa5;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +22,11 @@ public class NJStateMuseum extends AppCompatActivity {
 
         name = findViewById(R.id.museumName);
         name.setText(getResources().getText(R.string.museum_one));
+
+        Spinner spinner = (Spinner) findViewById(R.id.seniorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tickets_selected, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
 
