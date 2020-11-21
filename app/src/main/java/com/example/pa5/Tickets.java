b584fcbe5a925f84f9a9dd1f7574b967599afc2b
Race_Condition <40449073+RogueWorm@users.noreply.github.com>
2020-11-21 13:02:25 -0500

    Create Tickets.java

---
+++ app/src/main/java/com/example/pa5/Tickets.java
@@ -0,0 +1,30 @@
+package com.example.pa5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tickets extends AppCompatActivity {
+
+    int SPLASH_DELAY = 2; //(2 Second Delay)
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+        //Handler created to delay start screen with museum page
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Tickets.this, Museums.class));
+                finish();
+            }
+        }, SPLASH_DELAY * 1000);
+    }
+
+    //DISABLE BACK BUTTON
+    @Override
+    public void onBackPressed() {
+    }
+}
